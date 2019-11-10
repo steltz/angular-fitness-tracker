d1696502e850e77a9e7994f35132317e669845e0
Nicholas Stelter <steltz@Nicholass-MacBook-Pro.local>
2019-11-10 12:31:28 -0500

    setup app routing, create welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component'
+import { PastTrainingComponent } from './training/past-training/past-training.component';
+import { WelcomeComponent } from './welcome/welcome.component'
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { PastTrainingComponent } from './training/past-training/past-training.co
     TrainingComponent,
     CurrentTrainingComponent,
     NewTrainingComponent,
-    PastTrainingComponent
+    PastTrainingComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
