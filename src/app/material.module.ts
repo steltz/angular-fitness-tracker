242416326601064623555774ee9c4dd78326b02f
Nicholas Stelter <steltz@Nicholass-MacBook-Pro.local>
2019-11-12 08:04:13 -0500

    add date picker component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
+import { MatMomentDateModule } from '@angular/material-moment-adapter'
 import {
   MatButtonModule,
-  MatIconModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
   MatFormFieldModule,
+  MatIconModule,
   MatInputModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
 })
 export class MaterialModule {}
