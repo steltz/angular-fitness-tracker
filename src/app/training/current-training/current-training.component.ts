0ff0c880f8cf8c6d42ca68b1f9961fb5649f1db8
Nicholas Stelter <steltz@Nicholass-MacBook-Pro.local>
2019-11-25 07:49:28 -0500

    wireup stop training dialog

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { StopTrainingComponent } from './stop-training.component';
@@ -8,13 +8,19 @@ import { StopTrainingComponent } from './stop-training.component';
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.styl']
 })
+
 export class CurrentTrainingComponent implements OnInit {
+  @Output() exitTraining = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startTimer();
+  }
+
+  startTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       const stopTimer = this.progress >= 100;
@@ -25,8 +31,21 @@ export class CurrentTrainingComponent implements OnInit {
   }
 
   stopTraining() {
+    const { progress } = this;
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(exitTraining => {
+      if (exitTraining) {
+        this.exitTraining.emit();
+      } else {
+        this.startTimer();
+      }
+    });
   }
 
 }
