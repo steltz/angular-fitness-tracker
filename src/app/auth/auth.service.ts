1586a6ea836d9531ee0579aba67a8391ecb97bc8
Nicholas Stelter <steltz@Nicholass-MacBook-Pro.local>
2019-11-30 08:10:25 -0500

    implement global auth listener

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { Subject } from 'rxjs/Subject';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -20,13 +19,27 @@ export class AuthService {
     private trainingService: TrainingService
   ) {}
 
+  initAuthListener() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
+      } else {
+        this.trainingService.cancelSubscriptions();
+        this.authChange.next(false);
+        this.router.navigate(['/login']);
+        this.isAuthenticated = false;
+      }
+    });
+  }
+
   async registerUser(authData: AuthData) {
     const { email, password } = authData;
     try {
       await this.afAuth
         .auth
         .createUserWithEmailAndPassword(email, password);
-      this.authSuccess();
     } catch (err) {
       console.log(err);
     }
@@ -38,27 +51,16 @@ export class AuthService {
       await this.afAuth
         .auth.
         signInWithEmailAndPassword(email, password);
-      this.authSuccess();
     } catch (err) {
       console.log(err);
     }
   }
 
   logout() {
-    this.trainingService.cancelSubscriptions();
     this.afAuth.auth.signOut();
-    this.authChange.next(false);
-    this.router.navigate(['/login']);
-    this.isAuthenticated = false;
   }
 
   isAuth() {
     return this.isAuthenticated;
-  }
-
-  private authSuccess() {
-    this.isAuthenticated = true;
-    this.authChange.next(true);
-    this.router.navigate(['/training']);
   }
 }
